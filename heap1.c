b49915a674ecaaa79a715bacccd6da8a696bbd20
uenop <dummy@mail.com>
2012-06-01 02:44:57 +0900

    heap sort

--- heap1.c
+++ heap1.c
@@ -3,10 +3,10 @@
 #include <malloc.h>
 #include <time.h>
 
-#define N 100
+#define N 1000000
 #define NOTFOUND -1
 
-typedef struct {                /* 要はただの配列 */
+typedef struct {
     int q[N+1];                 /* index: 1,2,...,N */
     int n;
 } heap;
@@ -22,7 +22,6 @@ void bubble_up (int n, heap *H) {
 
     if (this < parent) {
 //        printf("bubble up %d < %d\n", H->q[n], H->q[n/2]);
-
         (H->q)[n] = parent;
         (H->q)[n/2] = this;
         bubble_up(n/2, H);
@@ -45,13 +44,17 @@ void bubble_down (int n, heap *H) {
     left = (2*n <= H->n) ? H->q[2*n] : min+1;
     right = (2*n+1 <= H->n) ? H->q[2*n + 1] : min+1;
 
+//    printf("[%d, %d, %d, %d] -- bubbledown\n", n, min, left, right);
+
     if (left < min)
         min = left;
     if (right < min)
         min = right;
 
-    if (min == n)
+    if (min == H->q[n]) {
+//        printf("stop bubbling down\n");
         return;
+    }
     else if (min == left) {
         temp = H->q[n];
         H->q[n] = H->q[2*n];
@@ -65,20 +68,26 @@ void bubble_down (int n, heap *H) {
     }
 }
 
-void delete (int n, heap *H) {
+void delete (int idx, heap *H) {
+    if (H->n < idx) {
+        printf("Error: H->n < idx -- delete\n");
+        return;
+    }
 
+    H->q[idx] = H->q[H->n];
+    bubble_down(idx, H);
+    H->n -= 1;
 }
 
-int search (int n, heap *H) {
-    /* O(N) */
-    int i;
-    for (i=1; i<N+1; i++)
-        if ((H->q)[i] == n)
-            return(i);
+int extract_min(heap *H) {
+    int min = NOTFOUND;
 
-    return(NOTFOUND);
+    if (H->n <= 0) return(min);
+
+    min = H->q[1];
+    delete(1, H);
+    return(min); // for debug
 }
-
 
 heap *make_heap1 (int *a, int n) {
     heap *H;
@@ -92,7 +101,17 @@ heap *make_heap1 (int *a, int n) {
     return H;
 }
 
-void main() {
+void heap_sort(int *a, int n) {
+    int i;
+    heap *H;
+
+    H = make_heap1(a, n);
+
+    for(i=0;i<n;i++)
+        a[i] = extract_min(H);
+}
+
+void test_delete() {
     heap *h;
     int a[N];
     int i;
@@ -102,17 +121,52 @@ void main() {
     for(i=0;i<N;i++)
         a[i] = rand()%N+1;
 
+    printf("\n** a **\n");
     for(i=0;i<N;i++) printf("%d ",a[i]);
     printf("\n");
 
     h = make_heap1(a, sizeof(a)/sizeof(a[0]));
 
-    printf("\n** make_heap **\n");
-    for (i=1;i<N+1;i++) {
-        if (i == 2 || i==4 || i==8 || i==16 || i==32 || i==64)
-            printf("\n");
-        printf("%d ",h->q[i]);
-    }
+    printf("\n** h->q **\n");
+    for(i=1;i<=h->n;i++) printf("%d ",h->q[i]);
+    printf("\n\nok\n\n");
+
+    printf("min: %d\n", h->q[1]);
+    printf("deleted: %d, n:%d\n", extract_min(h), h->n);
+    printf("min: %d\n", h->q[1]);
+    printf("deleted: %d, n:%d\n", extract_min(h), h->n);
+    printf("min: %d\n", h->q[1]);
+    printf("deleted: %d, n:%d\n", extract_min(h), h->n);
+    printf("min: %d\n", h->q[1]);
+
+    printf("\n** h->q **\n");
+    for(i=1;i<=h->n;i++) printf("%d ",h->q[i]);
     printf("\n");
 
+    return;
+}
+
+void test_heap_sort () {
+    heap *h;
+    int i;
+    int a[N];
+
+    srand(1);
+
+    for(i=0;i<N;i++)
+        a[i] = rand()%N+1;
+
+    printf("\n** a **\n");
+    for(i=0;i<N;i++) printf("%d ",a[i]);
+    printf("\n");
+
+    heap_sort(a, sizeof(a)/sizeof(a[0]));
+    printf("\n** heap_sort **\n");
+    for(i=0;i<N;i++) printf("%d ",a[i]);
+    printf("\n");
+
+}
+
+void main () {
+    test_heap_sort();
 }
